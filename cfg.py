96f7ad22c60f2158d210ffa83e37554917ab814a
shyamodule <70153993+shyamodule@users.noreply.github.com>
2020-08-26 13:55:47 +0530

    v1

--- cfg.py
+++ cfg.py
@@ -1,6 +1,16 @@
 
 from datetime import timedelta
 config = {
+
+    # mongoDB SRV link
+    'srv': "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]",
+
+    # database name
+    'db': 'testdb',
+
+    #collection name
+    'coll': 'test',
+
      # after this much time, the scrapping will start from begining
     'subtract': timedelta(days=1, hours=0, minutes=0),
 
@@ -15,10 +25,10 @@ config = {
     'media_size': 4e6, #roughly 4MB
 
      #sleep time between two cycles
-    'sleep_time': 5,
+    'sleep': 5,
 
      #max no. of documents to be inserted in database
-    'data_limit': 5000,
+    'database_limit': 5000,
 
     'root': 'https://flinkhub.com',
 
